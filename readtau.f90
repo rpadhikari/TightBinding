3a6b9e7ea2ff38a8bff494ee09cbd789c241be06
Rajendra Adhikari <rpadhikari@gmail.com>
2021-11-21 08:08:07 +0545

    Update readtau.f90

--- readtau.f90
+++ readtau.f90
@@ -1,10 +1,9 @@
-subroutine readtau()
+subroutine readtau(a, tau1, tau2, tau3)
   implicit none
   integer(4) :: i
   real(8) tau1(3), tau2(3), tau3(3) ! lattice vectors
   real(8) a ! lattice constant
   open(25, file='tau.dat', status='old', action='read')
-  read(25,*) a
   read(25,*) (tau1(i), i=1,3)
   read(25,*) (tau2(i), i=1,3)
   read(25,*) (tau3(i), i=1,3)
